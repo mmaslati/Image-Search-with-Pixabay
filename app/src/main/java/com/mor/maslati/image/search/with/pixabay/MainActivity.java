791df7c99d38e22eb9f17d982098433a97898c7e
Mor Maslati <mor.maslati@gmail.com>
2019-11-25 18:28:43 +0200

    Search is ready to add a Grid for results.

--- app/src/main/java/com/mor/maslati/image/search/with/pixabay/MainActivity.java
+++ app/src/main/java/com/mor/maslati/image/search/with/pixabay/MainActivity.java
@@ -14,47 +14,65 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Network network;
+
+    private Button button;
+    private FloatingSearchView mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FloatingSearchView mSearchView = findViewById(R.id.floating_search_view);
+        // Network Module for network calls.
+        network = new Network(this);
 
-        mSearchView.setOnSearchListener(new FloatingSearchView.OnSearchListener() {
-            @Override
-            public void onSuggestionClicked(SearchSuggestion searchSuggestion) {
-
-                Log.d("Mor","onSuggestionClicked( searchSuggestion ="+searchSuggestion+"  )");
-            }
-
-            @Override
-            public void onSearchAction(String currentQuery) {
-
-
-                Log.d("Mor","onSearchAction( currentQuery ="+currentQuery+"  )");
-            }
-        });
-
-
-        mSearchView.setOnQueryChangeListener(new FloatingSearchView.OnQueryChangeListener() {
-            @Override
-            public void onSearchTextChanged(String oldQuery, final String newQuery) {
-
-                Log.d("Mor","Something");
-                //get suggestions based on newQuery
-
-                //pass them on to the search view
-                //mSearchView.swapSuggestions(newSuggestions);
-            }
-        });
-
-
+        // Visual items creation.
+        mSearchView    = findViewById(R.id.floating_search_view);
+        button         = findViewById(R.id.searchButton);
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
 
+        // On Click Listener for Search-Button.
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Dismiss keyboard.
+                InputMethodManager imm = (InputMethodManager)getSystemService(getApplicationContext().INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(mSearchView.getWindowToken(), 0);
+
+                // Make the network call.
+                network.getImagesByQueryString(mSearchView.getQuery(), 1);
+            }
+        });
+
+
+        // On Click Listener for Search
+        mSearchView.setOnSearchListener(new FloatingSearchView.OnSearchListener() {
+
+            @Override
+            public void onSearchAction(String currentQuery) {
+                //Log.d("Mor","onSearchAction( currentQuery ="+currentQuery+"  )");
+
+                // Make the network call.
+                network.getImagesByQueryString(currentQuery, 1);
+            }
+
+            @Override
+            public void onSuggestionClicked(SearchSuggestion currentText) {
+                //Log.d("Mor","onSuggestionClicked( currentText ="+currentText+"  )");
+            }
+        });
+
+    }
 }
---
+++ app/src/main/java/com/mor/maslati/image/search/with/pixabay/Network.java
@@ -0,0 +1,98 @@
+package com.mor.maslati.image.search.with.pixabay;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class Network {
+
+    private Context context;
+    private String key;
+    private final String apiUrl = "https://pixabay.com/api/";
+
+    public Network( Context context ) {
+
+        this.context = context;
+        this.key     = context.getString(R.string.pixabay );
+    }
+
+    // Helper For all API Calls
+    private void sendIt(String urlString, Response.Listener successListener, Response.ErrorListener failedListener){
+
+        RequestQueue queue = Volley.newRequestQueue(this.context);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, urlString,successListener, failedListener );
+
+        queue.add(stringRequest);
+    }
+
+    // Attempting to Parse successful response.
+    private void parseImagesJSON(String fullImagesAsJSON){
+
+        ArrayList<JSONObject> images = new ArrayList<>();
+
+        JSONObject reader;
+
+        try {
+            reader = new JSONObject(fullImagesAsJSON);
+
+            JSONArray hits = (JSONArray) reader.get("hits");
+
+            for (int i = 0; i < hits.length(); i++) {
+
+                JSONObject image = hits.getJSONObject(i);
+
+                images.add(image);
+            }
+
+            // Todo: Connect answer from here to GridView.
+            Log.d("Mor","Finished");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void getImagesByQueryString( String query, int pageNumber ){
+
+        Response.Listener successGettingImages = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                parseImagesJSON( response );
+
+            }
+        };
+
+        Response.ErrorListener failedGettingImages = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error:",error.toString());
+            }
+        };
+
+
+        query = query.replaceAll(" ","+");
+
+        String getImagesWithQueryUrl = apiUrl+"?key="+key+"&q="+query+"&page="+pageNumber;
+
+        Log.d("Mor",getImagesWithQueryUrl);
+
+        sendIt(getImagesWithQueryUrl,successGettingImages,failedGettingImages);
+    }
+
+}
